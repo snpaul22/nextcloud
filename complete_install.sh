1d55d03c2e9e236b40a6e45d89db1bcf5ebf5337
snpaul22 <92539849+snpaul22@users.noreply.github.com>
2023-09-12 19:09:06 -0400

    Update complete_install.sh
    add PHP 8.2 repository

--- complete_install.sh
+++ complete_install.sh
@@ -12,6 +12,8 @@ COLLABORA_ADMIN_PASSWORD="your_collabora_admin_password"
 # Update system packages
 sudo apt update
 sudo apt upgrade -y
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
 
 # Install required packages
 sudo apt install -y apache2 mysql-server php8.2 php8.2-mysql php8.2-curl php8.2-gd php8.2-intl php8.2-json php8.2-mbstring php8.2-xml php8.2-zip libapache2-mod-php8.2 redis-server
